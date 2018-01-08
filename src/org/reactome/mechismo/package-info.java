6bef7a5232903e1abe4013d3f3923f06e7146d29
wug <wug@10.0.0.37>
2018-01-08 08:56:29 -0800

    Deleted the package moved to another project

--- src/org/reactome/mechismo/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package is used to group classes related to Mechismo data processing. Classes
- * related to Mechismo data analysis is not here. See the modeling document mechismo.vpp
- * in the model directory for the class structure.
- * @author wug
- *
- */
-package org.reactome.mechismo;
