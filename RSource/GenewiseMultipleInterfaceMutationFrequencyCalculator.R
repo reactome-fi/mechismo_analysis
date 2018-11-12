9ea64cb102e816a80d6e946824e060c50a8ea14b
Joshua Burkhart <burkhart.joshua@gmail.com>
2018-11-11 23:32:55 -0800

    using fisher test

--- RSource/GenewiseMultipleInterfaceMutationFrequencyCalculator.R
+++ RSource/GenewiseMultipleInterfaceMutationFrequencyCalculator.R
@@ -1,7 +1,7 @@
 library(magrittr)
 
 MECH_INPUT <- "/Users/joshuaburkhart/Downloads/TCGA_mech_input.tsv"
-MECH_OUTPUT <- "/Users/joshuaburkhart/Downloads/tcga_mechismo_stat_pancancer_undirectesd.tsv"
+MECH_OUTPUT <- "/Users/joshuaburkhart/Downloads/tcga_mechismo_stat_pancancer_undirected_significant.tsv"
 
 if(!file.exists("mech_input_df.rda")){
   mech_input_df <- read.delim(MECH_INPUT,
@@ -13,7 +13,8 @@ if(!file.exists("mech_input_df.rda")){
 
 if(!file.exists("mech_output_df.rda")){
   mech_output_df <- read.delim(MECH_OUTPUT,
-                               stringsAsFactors = FALSE)
+                               stringsAsFactors = FALSE,
+                               header = FALSE)
   save(mech_output_df,file="mech_output_df.rda")
 }else{
   load("mech_output_df.rda")
@@ -22,12 +23,12 @@ if(!file.exists("mech_output_df.rda")){
 if(!file.exists("gene_interfaces.rda")){
   gene_interfaces <- list()
   for(i in 1:nrow(mech_output_df)){
-    interface_samples_string <- mech_output_df[i,"Unique.Samples"]
+    interface_samples_string <- mech_output_df[i,15]
     interface_samples <- stringr::str_extract_all(interface_samples_string,
                                                   "TCGA-[0-9A-Z]{2}-[0-9A-Z]{4}-[0-9A-Z]{2}") %>%
       unlist()
-    interface_genes <- c(mech_output_df[i,"Partner.A"],
-                         mech_output_df[i,"Partner.B"]) %>%
+    interface_genes <- c(mech_output_df[i,1],
+                         mech_output_df[i,2]) %>%
       sort()
     
     cur_1_interfaces <- list()
@@ -89,7 +90,7 @@ gene_interface_mutation_distribution_df <- data.frame("Gene" = character(),
                                                       "Num.Singles.Samples" = numeric(),
                                                       "Num.Multis.Samples" = numeric(),
                                                       "Num.Interfaces" = numeric(),
-                                                      "Single.v.Multi.Wilcox" = numeric())
+                                                      "Single.v.Multi.Fisher" = numeric())
 for(i in 1:length(genes)){
   gene <- genes[[i]]
   multi_samples <- multi_gene_samples_hash[[gene]][["multi"]]
@@ -99,43 +100,45 @@ for(i in 1:length(genes)){
     if(length(single_samples) > 0){
       interfaces <- names(gene_interfaces[[gene]])
       if(length(interfaces) > 1){
-        singles_inteface_ratios <- numeric()
-        multis_interface_ratios <- numeric()
+        singles_inteface_counts <- numeric()
+        multis_interface_counts <- numeric()
         for(j in 1:length(interfaces)){
           interface <- interfaces[j]
           interface_singles <- intersect(gene_interfaces[[gene]][[interface]],
                                          single_samples)
           interface_multis <- intersect(gene_interfaces[[gene]][[interface]],
                                         multi_samples)
-          singles_ratio <- length(interface_singles) / length(single_samples)
-          multis_ratio <- length(interface_multis) / length(multi_samples)
-          singles_inteface_ratios <- c(singles_inteface_ratios,
-                                       singles_ratio)
-          multis_interface_ratios <- c(multis_interface_ratios,
-                                       multis_ratio)
+          singles_count <- length(interface_singles)
+          multis_count <- length(interface_multis)#doubles aren't counted twice but should be
+          singles_inteface_counts <- c(singles_inteface_counts,
+                                       singles_count)
+          multis_interface_counts <- c(multis_interface_counts,
+                                       multis_count)
+          if(length(singles_inteface_counts) != length(multis_interface_counts)){
+            print("ERROR: interface counts should match")
+            break
+          }
         }
         
-        if(length(singles_inteface_ratios) > 1 &
-           length(multis_interface_ratios) > 1){
-          normalized_singles_interface_ratios <- singles_inteface_ratios
-          if(max(singles_inteface_ratios) > 0){
-            normalized_singles_interface_ratios <- singles_inteface_ratios / max(singles_inteface_ratios)
-          }
-          normalized_multis_interface_ratios <- multis_interface_ratios
-          if(max(multis_interface_ratios) > 0){
-            normalized_multis_interface_ratios <- multis_interface_ratios / max(multis_interface_ratios)
-          }
+        if(length(singles_inteface_counts) > 1 &
+           length(singles_inteface_counts) == length(multis_interface_counts) &
+           sum(singles_inteface_counts) > 0 &
+           sum(multis_interface_counts) > 0){ # fisher test requires 2 or more non-zero row marginals
+          fisher.prep <- rbind(singles_inteface_counts,
+                               multis_interface_counts)
+          if(sum(as.numeric(colSums(fisher.prep) > 1)) > 1){
+          
           tmp_df <- data.frame("Gene" = gene,
                                "Num.Singles.Samples" = length(single_samples),
                                "Num.Multis.Samples" = length(multi_samples),
                                "Num.Interfaces" = length(interfaces),
-                               "Single.v.Multi.Wilcox" = wilcox.test(x = normalized_singles_interface_ratios,
-                                                                     y = normalized_multis_interface_ratios,
-                                                                     paired = TRUE,
-                                                                     alternative = "two.sided")$p.value)
+                               "Single.v.Multi.Fisher" = fisher.test(t(fisher.prep),
+                                                                     simulate.p.value = TRUE,
+                                                                     B = 1e4)$p.value)
           gene_interface_mutation_distribution_df <-
             rbind(gene_interface_mutation_distribution_df,
                   tmp_df)
+          }
         }
       }
     }
@@ -143,7 +146,7 @@ for(i in 1:length(genes)){
 }
 
 gene_interface_mutation_distribution_df %>%
-  dplyr::mutate(Single.v.Multi.Wilcox.BH.Adj.p = p.adjust(Single.v.Multi.Wilcox,method = "BH")) %>%
+  dplyr::mutate(Single.v.Multi.Fisher.BH.Adj.p = p.adjust(Single.v.Multi.Fisher,method = "BH")) %>%
   write.table("gene_interface_single_v_multi_mutation_distribution.tsv",
               row.names = FALSE,
               sep="\t")
